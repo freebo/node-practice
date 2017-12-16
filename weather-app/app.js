574c27a9e370e268d31df7ce3d665b807a648c8c
Mike Freeman <mike@Michaels-iMac.home.mikejfreeman.com>
2017-12-16 13:46:29 +1000

    Address lookup

---
+++ weather-app/app.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true, 
+            alias: 'address',
+            describe: 'Address to get weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('Can\'t connect');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Address not found');
+    } else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+});
+
+
