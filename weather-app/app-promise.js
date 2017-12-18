425dcc9730373ce68d69f3aec499ca08070d9232
Mike Freeman <mike@Michaels-iMac.local>
2017-12-18 16:13:02 +1000

    Weather promises

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -1,5 +1,7 @@
 
 const yargs = require('yargs');
+const axios = require('axios');
+
 
 var date = new Date();
 var current_hour = date.getHours();
@@ -19,5 +21,28 @@ const argv = yargs
     .alias('help','h')
     .argv;
 
+var encodedAddress = encodeURIComponent(argv.address);
 
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
+axios.get(geocodeUrl).then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Can\'t find address')
+    }
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    weatherUrl=`https://api.darksky.net/forecast/6116532b9310a3c3bea05f1df71a78b5/${lat},${lng}?units=ca`, 
+    console.log(`At ${current_hour}:${current_min} For address: ${response.data.results[0].formatted_address}`);
+    return axios.get(weatherUrl);
+}).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    var summary = response.data.currently.summary;
+    console.log(`Its Currently ${summary}, Temp: ${temperature} It Feels Like: ${apparentTemperature}`);
+}).catch((error) => {
+    if(error.code === 'ENOTFOUND') {
+    console.log('Can\'t connect to API');
+    } else {
+        console.log(error.message);
+    }
+});
