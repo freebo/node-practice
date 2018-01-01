014e6b3f1cfeea6d684a2b3b8848f5cbbb5d5078
Mike Freeman <freeman.mj@gmail.com>
2018-01-02 08:16:28 +1000

    A

--- weather-app/WeatherJS/index.js
+++ weather-app/WeatherJS/index.js
@@ -1,3 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://mjfhwork:QF6ib98KP8UCg4MvEdsmdONjYUwPcbChuwEYVaVECcCi00LwUIRfnzA9sItPJ7CHFjijGDespRXcloXXL1fRuA==@mjfhwork.documents.azure.com:10255/TodoApp?ssl=true", (err, db) => {
+    if (err) {
+        return context.log('Could not open db')
+    }
+    context.log('Connected to db');
+});
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -13,5 +22,6 @@ module.exports = function (context, req) {
             body: "Please pass a name on the query string or in the request body"
         };
     }
+}
     context.done();
-};
+
