d1647004ddf8f65df1092b48a747348c73ed6a50
Mike Freeman <freeman.mj@gmail.com>
2018-01-02 07:46:01 +1000

    a

--- todo-node-api/play/mogodb-connect.js
+++ todo-node-api/play/mogodb-connect.js
@@ -1,13 +1,20 @@
-// const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
 
 
+const MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://mjfhwork:QF6ib98KP8UCg4MvEdsmdONjYUwPcbChuwEYVaVECcCi00LwUIRfnzA9sItPJ7CHFjijGDespRXcloXXL1fRuA==@mjfhwork.documents.azure.com:10255/TodoApp?ssl=true";
 
-MongoClient.connect("mongodb://mjfhwork:QF6ib98KP8UCg4MvEdsmdONjYUwPcbChuwEYVaVECcCi00LwUIRfnzA9sItPJ7CHFjijGDespRXcloXXL1fRuA==@mjfhwork.documents.azure.com:10255/TodoApp?ssl=true", (err, db) => {
-    if (err) {
-        return console.log('Could not open db')
-    }
-    console.log('Connected to db');
+
+module.exports = function ((context, req) {
+    
+    MongoClient.connect(url, (err, db) => {
+        if (err) {
+            return context.log('Could not open db')
+        }
+        context.log('Connected to db');
+    });
+});
+
+
     // db.collection('Todos').insertOne({
     //     text: 'Something to do',
     //     completed: false
@@ -18,20 +25,18 @@ MongoClient.connect("mongodb://mjfhwork:QF6ib98KP8UCg4MvEdsmdONjYUwPcbChuwEYVaVE
     //     } 
     //     console.log(JSON.stringify(result.ops,undefined,2))
     // });
-
-    db.collection('Users').insertOne({
-        name: 'Mike Freeman',
-        age: 21,
-        location: 'Gold Coast'
-
-    }, (err,result) => {
-        if (err) {
-            return console.log ('Unable to insert User', err);
-        } 
-        console.log(JSON.stringify(result.ops,undefined,2))
-    });
-
-
-
-  db.close();
-});
+/*
+//     db.collection('Users').insertOne({
+//         name: 'Mike Freeman',
+//         age: 21,
+//         location: 'Gold Coast'
+//
+//     }, (err,result) => {
+//         if (err) {
+//             return console.log ('Unable to insert User', err);
+//         } 
+//         console.log(JSON.stringify(result.ops,undefined,2))
+//     });
+//   db.close();
+//
+*/
