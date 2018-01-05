b01db8c9eddfb8fc11d7d6a39d84e561599342ae
Mike Freeman <freeman.mj@gmail.com>
2018-01-05 15:05:25 +1000

    return auth token

--- todo-node-api/server/server.js
+++ todo-node-api/server/server.js
@@ -20,8 +20,8 @@ app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
-    todo.save().then((doc) => {
-        res.send(doc);
+    todo.save().then((user) => {
+        res.send(user);
 
     }, (e) => {
         res.status(400).send(e);
@@ -53,10 +53,23 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
-    
+
     if (!ObjectID.isValid(id)) {
         // console.log('Invalid ID');
         return res.status(404).send();
@@ -77,7 +90,7 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo});
 
     }).catch((e) => {
-        res.status(400).send();
+        res.status(400).send(e);
     })
 });
 
